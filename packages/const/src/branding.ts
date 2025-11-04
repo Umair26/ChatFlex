e205a3a821a6586eb7ae9a9f9cbf6f29f8b0e785
Umair Chauhan <umairyqb26@gmail.com>
2025-11-05 02:23:02 +0500

    Initial commit for Lobe Chat

--- packages/const/src/branding.ts
+++ packages/const/src/branding.ts
@@ -1,13 +1,10 @@
-// the code below can only be modified with commercial license
-// if you want to use it in the commercial usage
-// please contact us for more information: hello@lobehub.com
+export const LOBE_CHAT_CLOUD = 'ChatFlex Cloud';
 
-export const LOBE_CHAT_CLOUD = 'LobeHub Cloud';
+export const BRANDING_NAME = 'ChatFlex';
 
-export const BRANDING_NAME = 'LobeHub';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_LOGO_URL = '/logo.png';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'ChatFlex';
 
 export const BRANDING_URL = {
   help: undefined,
@@ -16,14 +13,14 @@ export const BRANDING_URL = {
 };
 
 export const SOCIAL_URL = {
-  discord: 'https://discord.gg/AYFPHvv2jT',
-  github: 'https://github.com/lobehub',
-  medium: 'https://medium.com/@lobehub',
-  x: 'https://x.com/lobehub',
-  youtube: 'https://www.youtube.com/@lobehub',
+  discord: '',
+  github: '',
+  medium: '',
+  x: '',
+  youtube: '',
 };
 
 export const BRANDING_EMAIL = {
-  business: 'hello@lobehub.com',
-  support: 'support@lobehub.com',
+  business: 'business@chatflex.com',
+  support: 'support@chatflex.com',
 };
